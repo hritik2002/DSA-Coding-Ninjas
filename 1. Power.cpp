f14c8350918580cd4befb310d39b23a698976362
Hritik Sharma <sharmahritik2002>
2022-03-01 00:22:15 +0530

    Adding Power.cpp in Recursion/ Recursion1

---
+++ 1. Power.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Power(int n , int p){
+    if(p<=0)
+    return 1;
+    return n*Power(n,p-1);
+}
+int main(){
+    int p , n ;
+    cout<<"Enter number , power"<<endl;
+    cin>>n>>p;
+    cout<<n<<" raise to "<<p<<" is "<<Power(n,p);
+}
