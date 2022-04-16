ea40d467c97b9a0fce79a106a0cc539f0ba2bbf8
hritik <hritik.sharma@mosaicwellness.in>
2022-04-16 19:49:49 +0530

    CheckAB.js

---
+++ Js/21.CheckAB.js
@@ -0,0 +1,43 @@
+/*
+Suppose you have a string, S, made up of only 'a's and 'b's. Write a recursive function that checks if the string was generated using the following rules:
+a. The string begins with an 'a'
+b. Each 'a' is followed by nothing or an 'a' or "bb"
+c. Each "bb" is followed by nothing or an 'a'
+If all the rules are followed by the given string, return true otherwise return false.
+Input format :
+String S
+Output format :
+'true' or 'false'
+Constraints :
+1 <= |S| <= 1000
+where |S| represents length of string S.
+Sample Input 1 :
+abb
+Sample Output 1 :
+true
+Sample Input 2 :
+abababa
+Sample Output 2 :
+false
+Explanation for Sample Input 2
+In the above example, a is not followed by either "a" or "bb", instead it's followed by "b" which results in false to be returned.
+*/
+
+
+function checkAB( input) {
+	// Write your code here
+    let check;
+    input = input.toString()
+    if(input.length===0)
+        return true;
+    if(input[0]!='a')
+        return false;
+    if(input[1]!='\0' && input[2]!='\0' && input[1]=='b' && input[2] == 'b'){
+         check = checkAB(input.split("").splice(3));
+    }
+    else{
+         check = checkAB(input.split("").splice(1));
+    }
+    return check;
+}
+console.log(checkAB("abba"))
